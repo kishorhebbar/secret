81314dfcdabd61959b99d5cb0814e896ca3cce9e
KishorHebbar <86525346+kishorhebbar@users.noreply.github.com>
2025-02-13 17:33:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-GITHUB_TOKEN=
+GITHUB_TOKEN=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
 
 aws_access_key = "AKIA1234567890EXAMPLE"
 aws_secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
